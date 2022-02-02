2d4bccb7a7172136cdab2f13ba60775d114ec0c6
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-02 14:56:34 -0300

    Setup test db env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,40 +1,18 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyA_pIdiLMKr-M7-8h-lDcAJkuOenngbFEw",
-    authDomain: "expensify-e0374.firebaseapp.com",
-    databaseURL: "https://expensify-e0374-default-rtdb.firebaseio.com",
-    projectId: "expensify-e0374",
-    storageBucket: "expensify-e0374.appspot.com",
-    messagingSenderId: "1019705217487",
-    appId: "1:1019705217487:web:d122c94f067ca4c36982ba",
-    measurementId: "G-V4B2FFNWT9"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABSE_URL,
+    projectId: process.env.FIREABSE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   firebase.initializeApp(config);
 
   const database = firebase.database();
 
-  database.ref().set({
-    name: 'Andrew Mead',
-    age: 26,
-    stressLevel: 6,
-    job: {
-      title: 'Software developer',
-      company: 'Google'
-    },
-    location: {
-        city: 'Philadelphia',
-        country: 'US'
-    }
-  }).then(() =>{
-    console.log('Data is saved!');
-  }).catch((error) => {
-    console.log('This failed: ', error);
-  });
-
-  database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
-  });
+  export { firebase, database as default }; 
