ffb6e27f559604da1f683bea16ce78cdae15acad
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-04 10:16:33 -0300

    File changes to check testing errors

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,27 +1,27 @@
 const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({
-            name: 'Luiggi',
-            age: 30
-        });
-        reject('Something went wrong!')
-    }, 1500);
+  setTimeout(() => {
+    resolve({
+      name: 'Andrew',
+      age: 26
+    });
+    // reject('Something went wrong!');
+  }, 5000);
 });
 
 console.log('before');
 
-promise.then((data) =>{
-    console.log('1', data);
+promise.then((data) => {
+  console.log('1', data);
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('This is my other promise');
-        }, 1500);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 5000);
+  });
 }).then((str) => {
-    console.log('Does this run?', str);
+  console.log('does this run?', str);
 }).catch((error) => {
-    console.log('error: ', error);
-})
+  console.log('error: ', error);
+});
 
 console.log('after');
